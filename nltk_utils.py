aa70f8e8bcb044ceded107efd79b55cd2d7e9217
ashgozli <ak23b24@gmail.com>
2023-01-01 15:44:33 -0800

    Fixed bugs, add NN Model & tested loss train

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,46 +1,43 @@
-import nltk
 import numpy as np
-#nltk.download('punkt')
+import nltk
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
-
-#Create a Stemmer
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
-    #Tokenizing each sentence
-    '''
+    """
     split sentence into array of words/tokens
     a token can be a word or punctuation character, or number
-    '''
-
+    """
     return nltk.word_tokenize(sentence)
 
+
 def stem(word):
-    '''
+    """
     stemming = find the root form of the word
     examples:
     words = ["organize", "organizes", "organizing"]
     words = [stem(w) for w in words]
     -> ["organ", "organ", "organ"]
-    '''
+    """
     return stemmer.stem(word.lower())
 
-def chunk_of_words(tokenized_sentence, all_words):
-    '''
+
+def bag_of_words(tokenized_sentence, words):
+    """
     return bag of words array:
     1 for each known word that exists in the sentence, 0 otherwise
     example:
     sentence = ["hello", "how", "are", "you"]
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
-    '''
-    #Stem each word in the sentence
-    tokenized_sentence = [stem(w) for w in tokenized_sentence]
-    #Initialize a bag with 0s for all words
-    bag = np.zeros(len(all_words), dtype=np.float32)
-    #Use enumarate to get index and the corresponding word 
-    for index, w in enumerate(all_words):
-        if w in tokenized_sentence:
-            bag[index] = 1.0
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
 
     return bag
--- train.py
+++ train.py
@@ -1,64 +1,116 @@
-import json
-from nltk_utils import tokenize, stem, chunk_of_words
 import numpy as np
+import random
+import json
+
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from nltk_utils import bag_of_words, tokenize, stem
+from model import NeuralNet
+
 with open('Ecommerce_Intents.json', 'r') as f:
     intents = json.load(f)
 
-#All words(x) and their corresponding tags(y) would be placed in these lists
 all_words = []
 tags = []
-
-#A list a tuple of words(x) and their tags(y) 
 xy = []
 
-#Loop through each sentence in the intents and patterns in json
+# loop through each sentence in our intents patterns
 for intent in intents['intents']:
-    tag = intent['responses']
+    tag = intent['tag']
+    # add to tag list
     tags.append(tag)
     for pattern in intent['patterns']:
+        # tokenize each word in the sentence
         w = tokenize(pattern)
+        # add to our words list
         all_words.extend(w)
+        # add to xy pair
         xy.append((w, tag))
 
-#Stem all words and disregard all the ignore_words specified 
-ignore_words = ['?', '!', '.']
+# stem and lower each word
+ignore_words = ['?', '.', '!']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
 all_words = sorted(set(all_words))
-tags = sorted(tags)
-print(tags)
+tags = sorted(set(tags))
 
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique stemmed words:", all_words)
+
+# create training data
 X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
-    bag = chunk_of_words(pattern_sentence, all_words)
+    # X: bag of words for each pattern_sentence
+    bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-
+    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
     label = tags.index(tag)
-    y_train.append(label) # Only want class labels: CrossEntropyLoss
+    y_train.append(label)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-class ChatDataSet(Dataset):
+# Hyper-parameters 
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+print(input_size, output_size)
+
+class ChatDataset(Dataset):
+
     def __init__(self):
         self.n_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
 
-    #dataset[idx]
-    def __getitem__ (self, index):
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
-        
-    def __len__ (self):
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
         return self.n_samples
 
-#Hyperparameter
-batch_size = 8
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
 
-dataset = ChatDataSet()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        # Forward pass
+        outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
