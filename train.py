93d988b771c6dbe65522ef2a80939206b5f29ebe
ashgozli <ak23b24@gmail.com>
2023-01-01 17:41:03 -0800

    Added Chat Functionality & saved the trained data

--- train.py
+++ train.py
@@ -114,3 +114,17 @@ for epoch in range(num_epochs):
 
 
 print(f'final loss: {loss.item():.4f}')
+
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "all_words": all_words,
+    "tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. File save to {FILE}')
